be18b5a0242307f9d35b50aff8bf7bec24a79924
rainman-tech <reynflores890@gmail.com>
2021-06-19 18:25:10 +0800

    Added dagger-hilt and created services and notifications.

---
+++ app/src/main/java/com/rain/spotify_clone/SpotifyApplication.kt
@@ -0,0 +1,7 @@
+package com.rain.spotify_clone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpotifyApplication : Application()
--- app/src/main/java/com/rain/spotify_clone/others/Constants.kt
+++ app/src/main/java/com/rain/spotify_clone/others/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
 
     const val SONG_COLLECTION = "songs"
 
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
+
 }
