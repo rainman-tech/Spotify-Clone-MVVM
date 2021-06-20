26234f97ba29f6f96e9785172621dc728d8df26e
rainman-tech <reynflores890@gmail.com>
2021-06-20 22:11:59 +0800

    Finalized MusicService.kt, created Resource and Event class.

---
+++ app/src/main/java/com/rain/spotify_clone/others/Event.kt
@@ -0,0 +1,19 @@
+package com.rain.spotify_clone.others
+
+open class Event<out T>(private val data: T) {
+
+    var hasBeenHandled = false
+    private set
+
+    fun getContentIfNotHandled(): T? {
+        return if(hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            data
+        }
+    }
+
+    fun peekContent() = data
+
+}
